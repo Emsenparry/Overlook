c132381dba73dc101b9f435b74bf45e99ebe2279
Emma Nielsen <Emmanielsen843@gmail.com>
2023-08-22 14:36:50 +0200

    Header scss

--- src/Components/Partials/Header/Header.jsx
+++ src/Components/Partials/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import NavBar from "../../Navigation/NavBar";
 import './Header.scss'
+import { ReactComponent as OverlookLogo } from "../../../Assets/SVG/hotel-overlook-logo.svg";
 
 const Header = () => {
   return (
     <>
       <header className="header-main">
-        <img src="../../../Assets/SVG/hotel-overlook-logo.svg" alt="overlookLogo" />
+        <OverlookLogo />
         <NavBar />
       </header>
     </>
