580306fbbe3867055c646d04cb95989c9d7b0c60
Emma Nielsen <Emmanielsen843@gmail.com>
2023-08-22 21:27:42 +0200

    Title on hero image

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { HeroSlider } from './Carousel/Carousel'
+import { ReactComponent as SiteHeader } from "../../../Assets/SVG/Site Headers.svg";
+import './Home.scss'
 
 const Home = () => {
   return (
+    <div className='hero-header'>
+    <div className='title'>
+      <h1>VELKOMMEN TIL HOTEL OVERLOOK ONLINE</h1>
+     <SiteHeader /> 
+    </div>
     <HeroSlider />
+    </div>
   )
 }
 
