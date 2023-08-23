b6fba70fbedf8dc8815ebf02401f13c54bb4025f
Emma Nielsen <Emmanielsen843@gmail.com>
2023-08-23 23:46:28 +0200

    validation on login
    finally working

--- src/Components/Pages/Login/Login.jsx
+++ src/Components/Pages/Login/Login.jsx
@@ -2,22 +2,19 @@ import axios from "axios";
 import React from "react";
 import { useAuth } from "../../Providers/AuthProvider";
 import { FormProvider, useForm } from "react-hook-form";
-import "./Login.scss";
 import { ContentWrapper } from "../../ContentWrapper/ContentWrapper";
+import "./Login.scss"; // Make sure the correct path is used here
 
 const Login = () => {
   const { loginData, setLoginData } = useAuth();
-  console.log(loginData);
 
   const { register, handleSubmit, formState, reset } = useForm();
-  // Uddrag fejlmeddelelser fra form-tilstanden
   const { errors } = formState;
 
-  const submitHandle = async (e) => {
-    e.preventDefault();
+  const submitHandle = async (data) => {
     const formData = new URLSearchParams();
-    formData.append("username", e.target.form.username.value);
-    formData.append("password", e.target.form.password.value);
+    formData.append("username", data.username);
+    formData.append("password", data.password);
 
     const endpoint = `http://localhost:4000/login`;
     try {
@@ -30,13 +27,11 @@ const Login = () => {
 
   const handleSessionData = (data) => {
     if (data) {
-      // Når vi skriver ned er det json.stringify
       sessionStorage.setItem("token", JSON.stringify(data));
       setLoginData(data);
     }
   };
 
-  // LOG OUT
   const LogOut = () => {
     sessionStorage.removeItem("token");
     setLoginData("");
@@ -46,55 +41,52 @@ const Login = () => {
   return (
     <ContentWrapper title="Login">
       <FormProvider {...register}>
-      {!loginData ? (
-        <form
-          className="login-form"
-          onSubmit={handleSubmit(submitHandle)}
-          method="POST"
-        >
-          <div className="Login-info">
-            <h2>Login</h2>
-            <p>Indtast dit brugernavn og adgangskode for at logge ind</p>
-          </div>
+        {!loginData ? (
+          <form
+            className="login-form"
+            onSubmit={handleSubmit(submitHandle)}
+            method="POST"
+          >
+            <div className="Login-info">
+              <h2>Login</h2>
+              <p>Indtast dit brugernavn og adgangskode for at logge ind</p>
+            </div>
+            <div>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Brugernavn"
+                {...register("username", { required: "Indtast brugernavn!" })}
+              />
+            </div>
+            <span className="error">{errors.username?.message}</span>
+            <div>
+              <input
+                type="password"
+                name="password"
+                placeholder="Adgangskode"
+                {...register("password", { required: "Indtast adgangskode!" })}
+              />
+            </div>
+            <span className="error">{errors.password?.message}</span>
+            <div className="button-container">
+              <button className="login" type="submit">
+                Login
+              </button>
+              <button className="reset" type="button" onClick={() => reset()}>
+                Annuller
+              </button>
+            </div>
+          </form>
+        ) : (
           <div>
-            <input
-              type="text"
-              name="username"
-              placeholder="Brugernavn"
-              {...register("username", { required: "Indtast brugernavn!" })}
-            />
-          </div>
-          <span className="error">{errors.username?.message}</span>
-          <div>
-            <input
-              type="password"
-              name="password"
-              placeholder="Adganskode"
-              {...register("password", { required: "Indtast adgangskode!" })}
-            />
-          </div>
-          <span className="error">{errors.password?.message}</span>
-          <div className="button-container">
-            <button
-              className="login"
-              type="button"
-              onClick={(e) => submitHandle(e)}
-            >
-              Login
-            </button>
-            <button className="reset" type="button" onClick={() => reset()}>
-              Annuller
+            <p>Du er logget ind</p>
+            <button className="logged-in" type="button" onClick={LogOut}>
+              Log ud
             </button>
           </div>
-        </form>
-      ) : (
-        <div>
-          <p>Du er logget ind</p>
-          <button className="logged-in" type="button" onClick={LogOut}>
-            Log ud
-          </button>
-        </div>
-      )}
+        )}
       </FormProvider>
     </ContentWrapper>
   );
