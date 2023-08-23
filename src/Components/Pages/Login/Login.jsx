5801ab7d30f99137ee41bd666db8db6d6a4f01ce
Emma Nielsen <Emmanielsen843@gmail.com>
2023-08-23 18:23:42 +0200

    Log out

--- src/Components/Pages/Login/Login.jsx
+++ src/Components/Pages/Login/Login.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import React from "react";
+import { useAuth } from "../../Providers/AuthProvider";
 
 const Login = () => {
-  // const [loginData, setLoginData]
+  const { loginData, setLoginData } = useAuth();
+  console.log(loginData);
 
   const submitHandle = async (e) => {
     const formData = new URLSearchParams();
@@ -11,35 +13,51 @@ const Login = () => {
 
     const endpoint = `http://localhost:4000/login`;
     try {
-      const result = await axios.post(endpoint, formData)
+      const result = await axios.post(endpoint, formData);
       handleSessionData(result.data.access_token);
     } catch (err) {
       console.error(`Kunne ikke logge ind: ${err}`);
     }
   };
 
-  const handleSessionData = data => {
-    if(data) {
-      sessionStorage.setItem('token', JSON.stringify(data))
+  const handleSessionData = (data) => {
+    if (data) {
+      // Når vi skriver ned er det json.stringify
+      sessionStorage.setItem("token", JSON.stringify(data));
+      setLoginData(data);
     }
+  };
+
+  const LogOut = () => {
+    sessionStorage.removeItem('token')
+    setLoginData('')
   }
 
   return (
-    <form method="POST">
-      <div>
-        <label htmlFor="username">Brugernavn</label>
-        <input type="text" name="username" />
-      </div>
-      <div>
-        <label htmlFor="password">Adgangskode</label>
-        <input type="password" name="password" />
-      </div>
-      <div>
-        <button type="button" onClick={(e) => submitHandle(e)}>
-          Login
-        </button>
-      </div>
-    </form>
+    <>
+      {!loginData ? (
+        <form method="POST">
+          <div>
+            <label htmlFor="username">Brugernavn</label>
+            <input type="text" name="username" />
+          </div>
+          <div>
+            <label htmlFor="password">Adgangskode</label>
+            <input type="password" name="password" />
+          </div>
+          <div>
+            <button type="button" onClick={(e) => submitHandle(e)}>
+              Login
+            </button>
+          </div>
+        </form>
+      ) : (
+        <div>
+          <p>Du er logget ind</p>
+          <button type="button" onClick={() => LogOut()}>Log ud</button>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './Components/Styled/Theme';
+import { AuthProvider } from './Components/Providers/AuthProvider';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthProvider>
     <BrowserRouter>
-    <ThemeProvider theme={theme}>
     <App />
-    </ThemeProvider>
     </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
 
