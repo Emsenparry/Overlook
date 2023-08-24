12978ba2d731ce19e5c40ceb805dba1fa3315745
Emma Nielsen <Emmanielsen843@gmail.com>
2023-08-24 03:03:40 +0200

    added a resized img + starting on rooms component

--- src/Components/Pages/Rooms/Rooms.jsx
+++ src/Components/Pages/Rooms/Rooms.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import { ContentWrapper } from '../../ContentWrapper/ContentWrapper'
+import React from "react";
+import { ContentWrapper } from "../../ContentWrapper/ContentWrapper";
+import ImageHeader from "../../Partials/ImageHeader/ImageHeader";
+import { ReactComponent as SiteHeader } from "../../../Assets/SVG/Site Headers.svg";
+import "./Rooms.scss";
 
 const Rooms = () => {
   return (
     <ContentWrapper title="Værelser">
-    <div>Rooms</div>
+      <div className="hero-header2">
+        <div className="title2">
+          <h2>Hoteller og Destinationer</h2>
+          <SiteHeader />
+        </div>
+      </div>
+      <div className="ImageHeader">
+        <ImageHeader
+          img={require("../../../Assets/images/frankfurt-skyline-germany-resize.jpg")}
+        />
+      </div>
     </ContentWrapper>
-  )
-}
+  );
+};
 
-export default Rooms
+export default Rooms;
--- src/Components/Partials/ImageHeader/ImageHeader.jsx
+++ src/Components/Partials/ImageHeader/ImageHeader.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import './ImageHeader.scss'
+
 
 const ImageHeader = ({ img }) => {
   return (
